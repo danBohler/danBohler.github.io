92f3959c09c36a794179fda309d251edcbc987f0
= <dgarcia@biblioeteca.com>
2024-04-19 20:57:15 +0200

    Primera version con imagenes de prueba donde al hacer scroll cambia entre las diferentes imagenes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# danBohler.github.io
+# invitacion-web-boda
 Sencilla web flyer con la información necesaria para los invitados que asistan a la boda
---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var listContainer = document.querySelector(".list-container");
+    var numberOfSections = 4; // Número de tramos
+    var currentSection = 0; // Inicialmente en el primer tramo
+
+    // Evento de desplazamiento
+    listContainer.addEventListener("scroll", function() {
+        // Obtiene la posición de desplazamiento vertical actual
+        var scrollTop = listContainer.scrollTop;
+        // Muestra la posición de desplazamiento por consola
+        console.log("Posición de desplazamiento:", scrollTop);
+        // Calcula el tamaño de cada tramo
+        var sectionSize = listContainer.scrollHeight / numberOfSections;
+        // Determina la sección actual en base a la posición de desplazamiento
+        var newSection = Math.floor(scrollTop / sectionSize);
+        // Si la sección actual es diferente de la sección anterior, cambia la imagen
+        if (newSection !== currentSection) {
+            // Muestra la imagen correspondiente a la nueva sección
+            showImage(newSection);
+            // Actualiza la sección actual
+            currentSection = newSection;
+        }
+    });
+
+    // Función para mostrar la imagen correspondiente a la sección actual
+    function showImage(section) {
+        // Oculta todas las imágenes
+        var images = document.querySelectorAll(".background-section img");
+        images.forEach(function(image) {
+            image.style.opacity = 0;
+        });
+        // Muestra la imagen correspondiente a la sección actual
+        images[section].style.opacity = 1;
+    }
+});
+
+document.getElementById('nextImage').addEventListener('click', function() {
+    changeImage(1); // Avanza a la siguiente imagen
+});
+
+document.getElementById('prevImage').addEventListener('click', function() {
+    changeImage(-1); // Regresa a la imagen anterior
+});
+
+function changeImage(direction) {
+    const images = document.querySelectorAll('.background-section img');
+    let visibleIndex = 0; // Encuentra la imagen visible
+    images.forEach((img, index) => {
+        if (img.style.opacity === '1') {
+            visibleIndex = index;
+            img.style.opacity = 0; // Oculta la imagen actual
+        }
+    });
+
+    // Calcula el índice de la siguiente imagen basándose en la dirección
+    const nextIndex = (visibleIndex + direction + images.length) % images.length;
+    images[nextIndex].style.opacity = 1; // Muestra la siguiente imagen
+}
+
+
