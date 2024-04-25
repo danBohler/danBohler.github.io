47f553c7d1d39be8f4f6f62ef68beba8cdced198
= <dgarcia@biblioeteca.com>
2024-04-25 19:27:15 +0200

    mobile con fondo del cielo

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,16 +42,15 @@ function updateImageSources() {
 document.addEventListener("DOMContentLoaded", function () {
     var myScreenOrientation = window.screen.orientation;
     myScreenOrientation.lock("portrait");
-    
+
     const floatButton = document.getElementById('float-button');
     const uls = document.querySelectorAll('#scrollContainer ul'); // Selecciona todos los ul dentro del contenedor
     let currentIndex = 0; // Empieza por mostrar el primer ul
 
-    floatButton.addEventListener('click', function() {
+    floatButton.addEventListener('click', function () {
         uls[currentIndex].style.opacity = 0; // Oculta el ul actual
         currentIndex = (currentIndex + 1) % uls.length; // Incrementa el índice o vuelve al inicio
         uls[currentIndex].style.opacity = 1; // Muestra el siguiente ul
-        showImage(currentIndex);
         changeBackgroundGradient(currentIndex);
     });
 
@@ -69,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Función para mostrar la imagen correspondiente a la sección actual
     function showImage(section) {
         // Oculta todas las imágenes
-        var images = document.querySelectorAll(".background-section img");
+        var images = document.querySelectorAll(".background-section img.bgimage");
         images.forEach(function (image) {
             image.style.opacity = 0;
         });
@@ -81,37 +80,36 @@ document.addEventListener("DOMContentLoaded", function () {
     function changeBackgroundGradient(section) {
         var backgroundContainer = document.querySelector(".image-container");
         backgroundContainer.classList.remove("morning", "afternoon", "evening", "night");
+        var element;
         switch (section) {
             case 0:
                 backgroundContainer.classList.add("morning");
+                element = document.querySelector('.morning');
                 break;
             case 1:
                 backgroundContainer.classList.add("afternoon");
+                element = document.querySelector('.afternoon');
                 break;
             case 2:
                 backgroundContainer.classList.add("evening");
+                element = document.querySelector('.evening');
                 break;
             case 3:
                 backgroundContainer.classList.add("night");
+                element = document.querySelector('.night');
                 break;
             default:
                 backgroundContainer.classList.add("morning");
+                element = document.querySelector('.morning');
                 break;
         }
+        // var opacity = window.getComputedStyle(element, '::before').getPropertyValue('opacity');
+        // element.style.opacity = opacity == '0' ? '1' : '0';
+
+        // Agrega clase 'active' para hacer visible el ::before
+        setTimeout(() => {
+            backgroundContainer.classList.add("active");
+        }, 50); // Pequeño delay para asegurar que la transición se ejecuta tras cambiar de estado
+        showImage(section);
     }
 });
-
-function changeImage(direction) {
-    const images = document.querySelectorAll('.background-section img');
-    let visibleIndex = 0; // Encuentra la imagen visible
-    images.forEach((img, index) => {
-        if (img.style.opacity === '1') {
-            visibleIndex = index;
-            img.style.opacity = 0; // Oculta la imagen actual
-        }
-    });
-
-    // Calcula el índice de la siguiente imagen basándose en la dirección
-    const nextIndex = (visibleIndex + direction + images.length) % images.length;
-    images[nextIndex].style.opacity = 1; // Muestra la siguiente imagen
-}
