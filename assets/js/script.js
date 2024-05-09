7ea641eb4cc5dce59dcef5cdf4264cfb02073607
= <dgarcia@biblioeteca.com>
2024-05-09 21:49:41 +0200

    avances con backgrounds y medidas de dispositivos moviles

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,28 @@ function updateImageSources() {
     if (width >= 1000) {
         // Desktop
         morning.src = '/assets/images/desktop_landscape_morning.png';
+        afternoon.src = '/assets/images/desktop_landscape_morning.png';
         evening.src = '/assets/images/desktop_landscape_evening.png';
         night.src = '/assets/images/desktop_landscape_night.png';
         console.log("Desktop")
     } else if (width >= 621 && width <= 999 && height > width) {
         // Tablet Portrait
         morning.src = '/assets/images/tablet_landscape_morning.png';
+        afternoon.src = '/assets/images/tablet_landscape_morning.png';
         evening.src = '/assets/images/tablet_landscape_evening.png';
         night.src = '/assets/images/tablet_landscape_night.png';
         console.log("Tablet Portrait")
     } else if (width >= 621 && width <= 999) {
         // Tablet Landscape
         morning.src = '/assets/images/desktop_landscape_morning.png';
+        afternoon.src = '/assets/images/desktop_landscape_morning.png';
         evening.src = '/assets/images/desktop_landscape_evening.png';
         night.src = '/assets/images/desktop_landscape_night.png';
         console.log("Tablet Landscape")
     } else if (width < 620) {
         // Mobile
         morning.src = '/assets/images/mobile_landscape_morning.png';
+        afternoon.src = '/assets/images/mobile_landscape_morning.png';
         evening.src = '/assets/images/mobile_landscape_evening.png';
         night.src = '/assets/images/mobile_landscape_night.png';
         console.log("Mobile")
@@ -36,51 +40,39 @@ function updateImageSources() {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // var myScreenOrientation = window.screen.orientation;
-    // myScreenOrientation.lock("portrait");
+    const landscape_images = [
+        "/assets/images/mobile_landscape_morning.png",
+        "/assets/images/mobile_landscape_morning.png",
+        "/assets/images/mobile_landscape_evening.png",
+        "/assets/images/mobile_landscape_night.png"
+    ];
+    const sky_images = [
+        "/assets/images/mobile_sky_morning.png",
+        "/assets/images/mobile_sky_morning.png",
+        "/assets/images/mobile_sky_evening.png",
+        "/assets/images/mobile_sky_night.png"
+    ];
 
     const floatButton = document.getElementById('float-button');
-    const uls = document.querySelectorAll('#scrollContainer ul'); // Selecciona todos los ul dentro del contenedor
-    let currentIndex = 0; // Empieza por mostrar el primer ul
+    const uls = document.querySelectorAll('#scrollContainer ul');
+    let currentIndex = 0;
 
     floatButton.addEventListener('click', function () {
-        uls[currentIndex].style.opacity = 0; // Oculta el ul actual
-        currentIndex = (currentIndex + 1) % uls.length; // Incrementa el índice o vuelve al inicio
-        uls[currentIndex].style.opacity = 1; // Muestra el siguiente ul
+        uls[currentIndex].style.opacity = 0;
+        currentIndex = (currentIndex + 1) % uls.length;
+        uls[currentIndex].style.opacity = 1;
         changeBackgroundGradient(currentIndex);
+        changeBackgroundImage(currentIndex);
+        changeContent(currentIndex)
     });
 
-    // Simular un clic en el botón floatButton
-    function triggerButtonClick() {
-        floatButton.click();
-    }
-
-    var listContainer = document.querySelector(".list-container");
-    // Evento de desplazamiento
-    listContainer.addEventListener("scroll", function () {
-        // triggerButtonClick();
-    });
-
-    // Función para mostrar la imagen correspondiente a la sección actual
-    function showImage(section) {
-        // Oculta todas las imágenes
-        var images = document.querySelectorAll(".background-section img.bgimage");
-        images.forEach(function (image) {
-            image.style.opacity = 0;
-        });
-        // Muestra la imagen correspondiente a la sección actual
-        images[section].style.opacity = 1;
-    }
-
-    // Función para cambiar el fondo gradiente en función de la sección
+    // Función para cambiar el fondo gradiente y la imagen correspondiente a la sección
     function changeBackgroundGradient(section) {
         var backgroundContainer = document.querySelector(".background-section");
-        // Asegurarse de quitar la clase 'active' para resetear la opacidad antes de cambiar de sección
         backgroundContainer.classList.remove("active");
-    
-        // Retrasar la adición de la clase para la nueva sección para permitir que se "limpie" el estado anterior
+
         setTimeout(() => {
-            backgroundContainer.classList.remove("morning", "afternoon", "evening", "night");
+            backgroundContainer.classList.remove("morning", "evening", "night", "text-night");
             switch (section) {
                 case 0:
                     backgroundContainer.classList.add("morning");
@@ -93,14 +85,48 @@ document.addEventListener("DOMContentLoaded", function () {
                     break;
                 case 3:
                     backgroundContainer.classList.add("night");
+                    backgroundContainer.classList.add("text-night");
                     break;
                 default:
                     backgroundContainer.classList.add("morning");
                     break;
             }
-            // Ahora añadir la clase 'active' para comenzar la transición
             backgroundContainer.classList.add("active");
-            showImage(section);
         }, 200);
+    }
+
+    // Función para mostrar la imagen correspondiente a la sección actual
+    function changeBackgroundImage(section) {
+        const landscapeImage = document.getElementById("landscape_image");
+        const skyImage = document.getElementById("sky_image");
+        // Oculta la imagen actual
+        landscapeImage.style.opacity = 0;
+        skyImage.style.opacity = 0;
+        // Cambia la imagen al siguiente en el array
+        landscapeImage.src = landscape_images[section];
+        skyImage.src = sky_images[section];
+        // Muestra la nueva imagen con una transición
+        landscapeImage.style.opacity = 1;
+        skyImage.style.opacity = 1;
+    }
+
+    // Función para mostrar la imagen correspondiente a la sección actual
+    function changeContent(section) {
+        const listContainer = document.getElementById("list-container");
+        // Calcular la altura de cada sección dividiendo la altura del contenedor entre 4
+        const sectionHeight = listContainer.scrollHeight / 4;
+        // Función para desplazar el scroll a una posición específica de acuerdo a la sección
+        function scrollToSection(sectionIndex) {
+            // Calcular la posición de desplazamiento multiplicando la altura de la sección por el índice
+            const scrollTo = sectionHeight * sectionIndex;
+            
+            // Desplazar el scroll hasta la posición calculada
+            listContainer.scrollTop = scrollTo;
+        }
+        // Ejemplo de cómo usar la función para desplazar al usuario a la segunda sección
+        scrollToSection(section);
+        $("#list-container").animate({
+            scrollTop: $("#second_ul").offset().top},
+            'slow');
     }
 });
